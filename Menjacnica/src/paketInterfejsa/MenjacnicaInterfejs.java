3a043ef7e968d58ad961f72b3c9c68d0e2d2553e
aleksandarcolic22414 <acacolic@gmail.com>
2016-03-21 23:16:34 +0100

    Uveden je interfejs MenjacnicaInterfejs sa odredjenim metodama.

---
+++ Menjacnica/src/paketInterfejsa/MenjacnicaInterfejs.java
@@ -0,0 +1,9 @@
+package paketInterfejsa;
+
+public interface MenjacnicaInterfejs {
+	
+	public void dodajKursValuteZaOdredjeniDan();
+	public void obrisiKursValuteZaOdredjeniDan();
+	public void vratiKursValuteZaOdredjeniDan();
+	
+}
