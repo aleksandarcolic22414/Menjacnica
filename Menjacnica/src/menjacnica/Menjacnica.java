d71bfa9a09d20c7cfdc1b5d4eb6bf6cb2553ece4
aleksandarcolic22414 <acacolic@gmail.com>
2016-03-22 00:07:16 +0100

    Promenjena je specifikacija metoda interfejsa i izvrsena drugacija impl.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,25 +1,43 @@
 package menjacnica;
 
+import java.util.LinkedList;
+
+import kurs.Kurs;
 import paketInterfejsa.MenjacnicaInterfejs;
+import valute.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> listaValuta;
 	@Override
-	public void dodajKursValuteZaOdredjeniDan() {
-		// TODO Auto-generated method stub
-
+	public void dodajKursValuteZaOdredjeniDan(String nazivValute,String skraceniNazivValute,Kurs kurs) {
+		Valuta v = new Valuta();
+		v.setKurs(kurs);
+		v.setNazivValute(nazivValute);
+		v.setSkraceniNazivValute(skraceniNazivValute);
+		if(!listaValuta.contains(v))
+			listaValuta.add(v);
 	}
 
 	@Override
-	public void obrisiKursValuteZaOdredjeniDan() {
-		// TODO Auto-generated method stub
-
+	public void obrisiKursValuteZaOdredjeniDan(String nazivValute,String skraceniNazivValute,Kurs kurs) {
+		Valuta v = new Valuta();
+		v.setKurs(kurs);
+		v.setNazivValute(nazivValute);
+		v.setSkraceniNazivValute(skraceniNazivValute);
+		if(listaValuta.indexOf(v) == -1)
+			listaValuta.remove(v);
 	}
 
 	@Override
-	public void vratiKursValuteZaOdredjeniDan() {
-		// TODO Auto-generated method stub
-
+	public Kurs vratiKursValuteZaOdredjeniDan(String nazivValute,String skraceniNazivValute,Kurs kurs) {
+		Valuta v = new Valuta();
+		v.setKurs(kurs);
+		v.setNazivValute(nazivValute);
+		v.setSkraceniNazivValute(skraceniNazivValute);
+		if(listaValuta.indexOf(v) != -1)
+			return kurs;
+		else return null;
 	}
 
 }
--- Menjacnica/src/paketInterfejsa/MenjacnicaInterfejs.java
+++ Menjacnica/src/paketInterfejsa/MenjacnicaInterfejs.java
@@ -1,9 +1,11 @@
 package paketInterfejsa;
 
+import kurs.Kurs;
+
 public interface MenjacnicaInterfejs {
 	
-	public void dodajKursValuteZaOdredjeniDan();
-	public void obrisiKursValuteZaOdredjeniDan();
-	public void vratiKursValuteZaOdredjeniDan();
+	public void dodajKursValuteZaOdredjeniDan(String nazivValute,String skraceniNazivValute,Kurs kurs);
+	public void obrisiKursValuteZaOdredjeniDan(String nazivValute,String skraceniNazivValute,Kurs kurs);
+	public Kurs vratiKursValuteZaOdredjeniDan(String nazivValute,String skraceniNazivValute,Kurs kurs);
 	
 }
