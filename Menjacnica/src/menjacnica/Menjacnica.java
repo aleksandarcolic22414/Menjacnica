12a563ce6ae9bc987424804c12274cdef58a9c8d
aleksandarcolic22414 <acacolic@gmail.com>
2016-03-21 23:35:32 +0100

    Napravnjena je klasa Menjacnica koja implementira MenjacnicaInterfejs.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import paketInterfejsa.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursValuteZaOdredjeniDan() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursValuteZaOdredjeniDan() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void vratiKursValuteZaOdredjeniDan() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
