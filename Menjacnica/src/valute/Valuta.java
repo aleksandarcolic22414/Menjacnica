4c4bb312cde00d1e757b3622b898efe4424c2f7d
aleksandarcolic22414 <acacolic@gmail.com>
2016-03-21 22:54:23 +0100

    Prvi komit.
    Napravljeni su paketi kurs i valute i u njima klase Kurs i
    Valuta.Takodje su dodati odgovorajuci atributi kao i get i set metode za
    te atribute.

---
+++ Menjacnica/src/valute/Valuta.java
@@ -0,0 +1,30 @@
+package valute;
+
+import kurs.Kurs;
+
+public class Valuta {
+	
+	private String nazivValute;
+	private String skraceniNazivValute;
+	private Kurs kurs;
+	
+	public String getNazivValute() {
+		return nazivValute;
+	}
+	public void setNazivValute(String nazivValute) {
+		this.nazivValute = nazivValute;
+	}
+	public String getSkraceniNazivValute() {
+		return skraceniNazivValute;
+	}
+	public void setSkraceniNazivValute(String skraceniNazivValute) {
+		this.skraceniNazivValute = skraceniNazivValute;
+	}
+	public Kurs getKurs() {
+		return kurs;
+	}
+	public void setKurs(Kurs kurs) {
+		this.kurs = kurs;
+	}
+
+}
